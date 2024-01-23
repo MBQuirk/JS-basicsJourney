51fed30695ee46cb18471f49bad4736fdfe98fea
MBQuirk <shpongle604@gmail.com>
2024-01-23 16:00:39 +0200

    Sixth Exercises

---
+++ 21.10Lab/sumOfNumbers.js
@@ -0,0 +1,11 @@
+function sumOfNumbers(input) {
+    let num = Number(input[0]);
+    let sum = 0;
+
+    for(let i = 0; i < num; i++){
+        let digit = num[i];
+        console.log(digit)
+    }
+
+}
+sumOfNumbers(["1234"]);
